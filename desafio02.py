e8c04cc3b895a9becc1527d29401a9036ede0a33
ritzabeth-dev <guevararitzabeth8@gmail.com>
2025-05-12 12:15:59 -0300

    Add practice files for day 2

---
+++ desafio02.py
@@ -0,0 +1,4 @@
+day=input('Day=')
+month=input('Month=')
+year=input('Year=')
+print('You were born',day,'of',month,year,'right?')
---
+++ desafio03.py
@@ -0,0 +1,3 @@
+first=input('first number')
+second= input('second number')
+print('first' + 'second')
